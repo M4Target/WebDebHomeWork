2846319ef7b131de03f4832d6ea2485da0a50ca1
M4Target <104734300+M4Target@users.noreply.github.com>
2024-05-29 03:20:43 +0100

    Node NPM trial - Random & Heros Name generation

---
+++ Node - NPM/index.js
@@ -0,0 +1,10 @@
+// var generateName = require("sillyname");
+// import generateName from sillyname;
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}`);
+
+import {randomSuperhero} from 'superheroes';
+
+var heronames = randomSuperhero();
+console.log(`I am ${heronames}!`);
