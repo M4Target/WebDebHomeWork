230d1f15ddf6880676525c5e12d08b4b29fb3553
Marco M <mhinchun5@gmail.com>
2024-05-31 08:48:11 +0100

    Node until EJS partial learning

---
+++ Node - EJS Project one/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  const data = {
+    title: "EJS Tags",
+    seconds: new Date().getSeconds(),
+    items: ["apple", "banana", "cherry"],
+    htmlContent: "<strong>This is some strong text</strong>",
+  };
+  res.render("index.ejs", data);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+let bowl = ["Apples", "Oranges"];
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { fruits: bowl });
+});
